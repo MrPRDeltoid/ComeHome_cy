d55c217154d05459ed15961c13d6561d221d0386
Peter Manton <pcmanton@gmail.com>
2024-10-03 11:44:39 -0700

    Add test for Property Page Property Options Panel

--- cypress/pages/propertyPage.js
+++ cypress/pages/propertyPage.js
@@ -15,20 +15,45 @@ export class PublicView extends PropertyPage {
   URL = `${Cypress.config('baseUrl')}property-details`
 
   // Elements and methods specific to public view
+  // Photo Section
   photoSection() {return cy.getBySel("carousel-section")}
+  // Intro Section
   introSection() {return cy.getBySel("property-intro-section")}
-  introSectionAddress() {return cy.get('.PropertyIntro__Address')}
+  introSectionAddress() {return this.introSection().find('.PropertyIntro__Address')}
   introSectionDetails() {return cy.getBySel("property-info")}
+  // Property Details Section
   propertyDetailsSection() {return cy.get('[class$=__AdditionalHomeDetails]')}
   propertyDetailsTable() {return this.propertyDetailsSection().find('table')}
   propertyDetailsCaption() {return this.propertyDetailsSection().find('caption')}
   propertyDetailsRow() {return this.propertyDetailsTable().find('tr')}
   propertyDetailsAttribution() {return this.propertyDetailsSection().find('.AdditionalHomeDetails__Legal')}
+  // Summary Options Section
   summaryOptionsPanel() {return cy.getBySel("summary-options-panel")}
+  listingStatus() {return cy.getBySel("listing-status")}
+  listingStatusHeader() {return this.listingStatus().find('.PDPRightRailCard__ListingStatus')}
+  listingStatusMonthlyPayment() {return cy.getBySel("monthly-payment")}
+  listingStatusMortgageInfoButton() {return this.listingStatusMonthlyPayment().find('button')}
+  mortgageInfo() {return this.summaryOptionsPanel().find('.PDPRightRailCard__MortgageInfo')}
+  mortgageInfoLabel() {return this.mortgageInfo().find('[class$=__Label]')}
+  mortgageInfoValue() {return this.mortgageInfo().find('[class$=__Value]')}
+  getPreApprovedButton() {return this.summaryOptionsPanel().find('[data-event-name=click_lender_cta]')}
+  contactAgentButton() {return cy.getBySel("contact-agent-button")}
+  // TODO: Cannot chain mulitple find methods? So following Share/Save button needs top selector defined to get both button and label elements using find
+  shareButton() {return cy.getBySel("share-button")}
+  shareButtonButton() {return this.shareButton().find('button')}
+  shareButtonLabel() {return this.shareButton().find('label')}
+  saveButton() {return cy.getBySel("save-button")}
+  saveButtonButton() {return this.saveButton().find('button')}
+  saveButtonLabel() {return this.saveButton().find('.PDPRightRailCard__ButtonLabel')}
+  mlsAttribution() {return this.summaryOptionsPanel().find('.PDPRightRailCard__MLSAttribution')}
+  // Upsell Section
   upsellSection() {return cy.getBySel("upsell-section")}
   upsellSectionClaimHomeButton() {return cy.get('[data-event-name=click_property_details_owner_promo]')}
+  // Map Section
   mapViewSection() {return cy.get('section[class^=MapPropertyPage__MapPropertyPage]')}
+  // AVM Breakdown Section
   avmBreakdownSection() {return cy.getBySel("avm-breakdown")}
+  // AVM Breakout Section
   avmBreakoutSection() {return cy.getBySel("avm-breakout-section")}
   
   //Methods
---
+++ cypress/tests/property_page/property_options_panel.cy.js
@@ -0,0 +1,42 @@
+import { PublicView } from "../../pages/propertyPage"
+
+
+const public_view = new PublicView
+const property = 'property1'
+
+beforeEach(() => {
+  // Load the Property Page for the given property data
+  public_view.loadPropertyPage(property)
+})
+
+describe('The Options Panel', () => {
+  it('Has correct listing and mortgage information', function() {
+    const data = this.property_data[property]
+    public_view.listingStatusHeader().should('contain.text', "Off Market $")
+    public_view.listingStatusMonthlyPayment().should('contain.text', "Estimated monthly payment $")
+    public_view.listingStatusMortgageInfoButton().should('have.attr', 'aria-expanded', 'false')
+    public_view.mortgageInfo().should('not.exist')
+    // Click the mortgage info button to expand
+    public_view.listingStatusMortgageInfoButton().click()
+    public_view.mortgageInfo().should('exist')
+    // TODO: Research a better way to do this
+    const label_exp = ['Year fixed', 'Rate', 'Mortgage Payment', 'Property taxes', 'Insurance', 'HOA fees']
+    public_view.mortgageInfoLabel().each(($label, index) => {
+        cy.wrap($label).should('have.text', label_exp[index])
+    })
+    const value_exp = ['30', '%', '$', '$', '--', '--']
+    public_view.mortgageInfoValue().each(($value, index) => {
+        cy.wrap($value).should('contain.text', value_exp[index])  // Using contain, as values will change
+    })
+    // Click to hide mortgage info
+    public_view.listingStatusMortgageInfoButton().click()
+    public_view.mortgageInfo().should('not.exist')
+    public_view.getPreApprovedButton().should('have.text', "Get pre-approved").and('be.enabled')
+    public_view.contactAgentButton().should('have.text', "Contact Agent").and('be.enabled')
+    public_view.shareButtonButton().should('be.enabled').and('have.attr', 'aria-label', "share")
+    public_view.shareButtonLabel().should('have.text', "Share")
+    public_view.saveButtonButton().should('be.enabled').and('have.attr', 'aria-label', "Save this property to your Watchlist")
+    public_view.saveButtonLabel().should('have.text', "Save")
+    public_view.mlsAttribution().should('have.text', "Courtesy of RE/MAX Gold Coast REALTORS")
+  })
+})
